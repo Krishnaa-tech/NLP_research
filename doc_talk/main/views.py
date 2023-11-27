05090453f92a0a2a62e658b6af3e81a7687666cf
krishnaa-tech <goyal.krish0522@gmail.com>
2023-11-27 22:46:43 +0530

    update

--- doc_talk/main/views.py
+++ doc_talk/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . import utils
+from .models import Article
+from transformers import AutoTokenizer, PegasusForConditionalGeneration
 
 # Create your views here:
 def home(request): 
@@ -21,6 +23,20 @@ def process_uploaded_file(uploaded_file_path):
         print("Unsupported file format. Please upload a PDF or a text file.")
         return None
   
+# def document(request):
+#     if request.method == 'POST' and request.FILES['uploaded_file']:
+#         uploaded_file = request.FILES['uploaded_file']
+#         file_path = process_uploaded_file(uploaded_file)
+
+#         if file_path:
+#             text_content = process_uploaded_file(file_path)
+#             return render(request, 'ner.html', {'text_content': text_content})
+#         else:
+#             error_message = "Unsupported file format. Please upload a PDF or a text file."
+#             return render(request, 'ner.html', {'error_message': error_message})
+#     else:
+#         return render(request, 'main/ner.html')    
+    
 def document(request):
     if request.method == 'POST' and request.FILES['uploaded_file']:
         uploaded_file = request.FILES['uploaded_file']
@@ -33,7 +49,8 @@ def document(request):
             error_message = "Unsupported file format. Please upload a PDF or a text file."
             return render(request, 'ner.html', {'error_message': error_message})
     else:
-        return render(request, 'main/ner.html')    
+        return render(request, 'main/ner.html')
+
     
 def ner(request): 
     return render(request, 'main/ner.html')
@@ -58,4 +75,28 @@ def underline_words(request):
     return render(request, 'main/ner.html', {'input_text': input_text, 'output_text': output_text})
 
 
-    
+def generate_summary(article_text):
+    # Load pre-trained model and tokenizer
+    model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
+    tokenizer = AutoTokenizer.from_pretrained("google/pegasus-xsum")
+
+    # Tokenize the input text
+    inputs = tokenizer(article_text, max_length=1024, return_tensors="pt")
+
+    # Generate Summary
+    summary_ids = model.generate(inputs["input_ids"])
+
+    # Decode and return the summary
+    summary = tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    return summary
+
+def summarize_article(request, article_id):
+    # Retrieve the article from the database
+    article = Article.objects.get(pk=article_id)
+
+    # Generate the summary using the function
+    summary = generate_summary(article.content)
+
+    # Pass the summary to the template
+    context = {'article': article, 'summary': summary}
+    return render(request, 'ner.html', context)
