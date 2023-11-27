22afebac16cd548e07531dee5bce849cae73e4a3
Joan Job <129768305+JOE1904@users.noreply.github.com>
2023-11-27 12:56:01 +0530

    Edits in view.py and new html page.

--- doc_talk/main/utils.py
+++ doc_talk/main/utils.py
@@ -1,34 +1,36 @@
-import PyPDF2
+# import PyPDF2
+
+# from datetime import datetime
 import re 
-from datetime import datetime
 import spacy
 
 # Function to read PDF files and store the extracted text in a text file
-def read_pdf(file_path):
-    timestamp = datetime.now().strftime("Y%Y_M%m_D%dT_%H_%M_%S") 
-    cleaned_path = re.sub(r'[:.\/\\*\?"<>|]', '_', file_path)
-    new_text_file_name =  f"{cleaned_path}_output_{timestamp}.txt"
-    with open(file_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        text = ""
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
+# def read_pdf(file_path):
+#     timestamp = datetime.now().strftime("Y%Y_M%m_D%dT_%H_%M_%S") 
+#     cleaned_path = re.sub(r'[:.\/\\*\?"<>|]', '_', file_path)
+#     new_text_file_name =  f"{cleaned_path}_output_{timestamp}.txt"
+#     with open(file_path, 'rb') as pdf_file:
+#         pdf_reader = PyPDF2.PdfReader(pdf_file)
+#         text = ""
+#         for page_num in range(len(pdf_reader.pages)):
+#             page = pdf_reader.pages[page_num]
+#             text += page.extract_text()
 
-        text_to_txt = clean_text(text)
-    try:
-        with open(new_text_file_name, 'w', encoding='utf-8') as file:
-            file.write(text_to_txt)
-        print(f"Text has been successfully written to {new_text_file_name}")
-        return new_text_file_name
-    except Exception as e:
-        print(f"Error while writing to the file: {e}")
+#         text_to_txt = clean_text(text)
+#     try:
+#         with open(new_text_file_name, 'w', encoding='utf-8') as file:
+#             file.write(text_to_txt)
+#         print(f"Text has been successfully written to {new_text_file_name}")
+#         return new_text_file_name
+#     except Exception as e:
+#         print(f"Error while writing to the file: {e}")
 
         # Function to read text files and store the text in a variable
 def read_text(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as txt_file:
             text = txt_file.read()
+            text = clean_text(text)       
         return text
     except Exception as e:
         print(f"Error while processing the text file: {e}")
@@ -39,24 +41,27 @@ def clean_text(text):
     # Remove line breaks and extra spaces
     text = re.sub(r'\n+', ' ', text)
     text = re.sub(r'\s+', ' ', text)
-    
 
     # Remove unwanted characters
-    text = re.sub(r'[^A-Za-z0-9.,?!()\'":;\- ]', '', text)
-    
+    text = re.sub(r'[^A-Za-z0-9.,?!()\'":;\-]', ' ', text)
+
     # Add missing spaces after punctuation marks
     text = re.sub(r'([.,?!();:])', r'\1 ', text)
-    
+
     # Remove extra spaces after punctuation marks
-    text = re.sub(r' +([.,?!();:])', r'\1', text)
-    
+    text = re.sub(r'\s+([.,?!();:])', r'\1', text)
+
     # Remove spaces before punctuation marks
-    text = re.sub(r' ([.,?!();:])', r'\1', text)
-    
+    text = re.sub(r'\s([.,?!();:])', r'\1', text)
+
     # Remove spaces before and after hyphens
-    text = re.sub(r' - ', ' -', text)
-    text = re.sub(r' -', '-', text)
-    text = re.sub(r'- ', '-', text)
+    text = re.sub(r'\s-\s', '-', text)
+    text = re.sub(r'\s-', '-', text)
+    text = re.sub(r'-\s', '-', text)
+
+    # Trim leading and trailing spaces
+    text = text.strip()
+
     return text
 
 def extract_proper_noun_phrases(text):
--- doc_talk/main/views.py
+++ doc_talk/main/views.py
@@ -7,14 +7,14 @@ def home(request):
 
 # Function to identify and process the uploaded file
 def process_uploaded_file(uploaded_file_path):
-    if uploaded_file_path.endswith('.pdf'):
-        # It's a PDF, read and store the text in a text file
-        file_name = utils.read_pdf(uploaded_file_path)
-        with open(file_name, 'r', encoding='utf-8') as txt_file:
-            text = txt_file.readlines()
-        return text
+    # if uploaded_file_path.endswith('.pdf'):
+    #     # It's a PDF, read and store the text in a text file
+    #     file_name = utils.read_pdf(uploaded_file_path)
+    #     with open(file_name, 'r', encoding='utf-8') as txt_file:
+    #         text = txt_file.readlines()
+    #     return text
 
-    elif uploaded_file_path.endswith('.txt'):
+    if uploaded_file_path.endswith('.txt'):
         # It's a text file, read the text directly into a variable
         return utils.read_text(uploaded_file_path)
     else:
@@ -28,29 +28,31 @@ def document(request):
 
         if file_path:
             text_content = process_uploaded_file(file_path)
-            return render(request, 'index.html', {'text_content': text_content})
+            return render(request, 'ner.html', {'text_content': text_content})
         else:
             error_message = "Unsupported file format. Please upload a PDF or a text file."
-            return render(request, 'index.html', {'error_message': error_message})
+            return render(request, 'ner.html', {'error_message': error_message})
     else:
-        return render(request, 'index.html')    
+        return render(request, 'ner.html')    
     
 
 def underline_words(request):
-    if request.method == 'POST':
-        text = request.POST.get('input_text', '')
-        words_to_underline = request.POST.get('words_to_underline', '')
+    input_text = ''
+    output_text = ''
 
-        # Convert the comma-separated string of words to a list
-        words_to_underline_list = [word.strip() for word in words_to_underline.split(',')]
+    if request.method == 'POST':
+        input_text = request.POST.get('input_text', '')
+
+        # Extract proper noun phrases
+        words_to_underline_list = utils.extract_proper_noun_phrases(input_text)
 
         # Underline specified words in the text
         for word in words_to_underline_list:
-            text = text.replace(word, f'<u>{word}</u>')
+            input_text = input_text.replace(word, f'<u>{word}</u>')
 
-        return render(request, 'underline_words.html', {'input_text': text})
-    else:
-        return render(request, 'underline_words.html')
+        output_text = input_text  # You can modify this based on additional processing
+
+    return render(request, 'ner.html', {'input_text': input_text, 'output_text': output_text})
 
 
     
